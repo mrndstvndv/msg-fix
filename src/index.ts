6512560d3632fe984bc93a9b7ef4b95e801a13f8
mrndstvndv <stvndvmrnd@gmail.com>
2026-01-08 16:09:16 +0000

    Revert "removed url" removing url tag results in it using the original url
    This reverts commit 7b8982cded0f741d6371338be100949934144f36.

--- src/index.ts
+++ src/index.ts
@@ -43,6 +43,7 @@ export default {
 	<meta property="og:image:width" content="${image?.sizes.large?.w}">
 	<meta property="og:image:height" content="${image?.sizes.large?.h}">
 	<meta property="og:type" content="website">
+	<meta property="og:url" content="https://example.com">
 
 	<style>
 		body {
